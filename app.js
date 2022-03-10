2b34e83bbe8cfa1ec85b4dd15da90925ff2ded87
JonasMota <jonasmotafeitosa@gmail.com>
2022-03-10 17:34:22 -0300

    commit teste

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const server = express();
+const api = require('./api')
+
+server.use(express.json());
+
+server.listen(8081, ()=>{
+    console.log("servidor rodando https://localhost:8081")
+})
+
+server.get('/', (req, res) =>{
+    return res.send({message: "Jonas Mota "})
+})
+
+server.get('/pokemon/:id', async (req, res) =>{
+    const {id} = req.params
+    try {
+        const { data } = await api.get(`pokemon/${id}`)
+        return res.send ({name: data.name})
+    } catch (error) {
+        res.send({error: error.message})
+        
+    }
+})
